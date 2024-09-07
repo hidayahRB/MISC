b05757a0fd8f019c76ea17ea8ff3f495b374c886
Norhidayah <75527623+hidayahRB@users.noreply.github.com>
2024-09-08 02:24:56 +0800

    Update A1_DataTransformation.sql

--- A1_DataTransformation.sql
+++ A1_DataTransformation.sql
@@ -22,3 +22,14 @@ ALTER TABLE rental ADD COLUMN rental_duration INTERVAL;
 UPDATE rental
 SET rental_duration = return_date - rental_date
 WHERE return_date IS NOT NULL;
+
+
+-- Create additional calculated column of rental duration per rental transaction
+ALTER TABLE rental ADD COLUMN rental_due_date timestamp;
+
+UPDATE rental r
+SET rental_due_date = r.rental_date + INTERVAL '1 day' * f.rental_duration
+FROM inventory i
+JOIN film f ON i.film_id = f.film_id
+WHERE r.inventory_id = i.inventory_id
+  AND r.return_date IS NOT NULL;
