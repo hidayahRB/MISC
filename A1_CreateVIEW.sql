c01cd835167630b752c711ddcde7a82a7580432d
Norhidayah <75527623+hidayahRB@users.noreply.github.com>
2024-09-08 10:53:11 +0800

    Update A1_CreateVIEW.sql

--- A1_CreateVIEW.sql
+++ A1_CreateVIEW.sql
@@ -21,6 +21,10 @@ FROM RankedRentals
 WHERE rank <= 10
 ORDER BY rental_year, rank;
 
+-- Select the top 10 rented films of the latest year
+SELECT * FROM top_rented_films
+WHERE rental_year = (SELECT MAX(rental_year) FROM top_rented_films);
+
 
 -- Create a view of monthly revenue for each country
 CREATE VIEW monthly_revenue_country AS
@@ -36,16 +40,38 @@ JOIN country C ON city.country_id = C.country_id
 GROUP BY year, month, country
 ORDER BY country ASC;
 
+-- Select the monthly revenue by country
+SELECT * FROM monthly_revenue_country
+WHERE country = 'United States';
+
+-- Select the top 3 total revenue over all country in record
+SELECT 
+	country,
+	SUM(monthly_revenue) AS Total_revenue
+FROM monthly_revenue_country
+GROUP BY country
+ORDER BY Total_revenue DESC
+LIMIT 3;
+
 
 -- Create a DVD return status view
-CREATE VIEW dvd_return_status AS
-SELECT CASE
+CREATE VIEW dvd_rental_status AS
+SELECT 
+       f.film_id,
+	   f.title AS film_title,
+	   r.rental_id,
+       r.rental_date,
+	   r.return_date,
+	   CASE
            WHEN r.return_date IS NULL THEN 'Not Yet Returned'
            WHEN r.return_date <= r.rental_date + INTERVAL '1 day' * f.rental_duration THEN 'Returned Early'
            WHEN r.return_date > r.rental_date + INTERVAL '1 day' * f.rental_duration THEN 'Returned Late'
-       END AS return_status,
-	   COUNT(*) AS Total_film
+       END AS return_status
 FROM rental r
 JOIN inventory i ON r.inventory_id = i.inventory_id
-JOIN film f ON i.film_id = f.film_id
+JOIN film f ON i.film_id = f.film_id;
+
+-- Select the total of each dvd rental status
+SELECT return_status, COUNT(*) AS Total_film
+FROM dvd_rental_status
 GROUP BY return_status;
